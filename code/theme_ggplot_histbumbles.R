bde788dd15f18492f85c2f6fbac3e99946278212
jhemberger <you@example.com>
2020-10-30 11:06:05 -0700

    WIP: update models, play with quasi, created new Rmd for spp trends

---
+++ code/theme_ggplot_histbumbles.R
@@ -0,0 +1,88 @@
+library(ggplot2)
+library(gridExtra)
+library(grid)
+
+theme_histbumbles = function(base_size = 12, base_family = "") {
+  
+  theme_grey(base_size = base_size, base_family = base_family) %+replace%
+    
+    theme(
+      # Specify axis options
+      axis.line = element_blank(),
+      axis.text.x = element_text(
+        size = base_size * 0.8,
+        color = "black",
+        lineheight = 0.9
+      ),
+      axis.text.y = element_text(
+        size = base_size * 0.8,
+        color = "black",
+        lineheight = 0.9
+      ),
+      axis.ticks = element_line(color = "black", 
+                                size  =  0.8),
+      axis.title.x = element_text(
+        size = base_size,
+        face = "bold", 
+        color = "black",
+        margin = margin(0, 10, 0, 0)
+      ),
+      axis.title.y = element_text(
+        size = base_size,
+        face = "bold",
+        color = "black",
+        angle = 90,
+        margin = margin(0, 10, 0, 0)
+      ),
+      axis.ticks.length = unit(0.5, "lines"),
+      # Specify legend options
+      legend.background = element_blank(),
+      legend.key = element_blank(),
+      legend.key.size = unit(1, "lines"),
+      legend.key.height = NULL,
+      legend.key.width = NULL,
+      legend.text = element_text(size = base_size * 0.8, 
+                                 color = "black"),
+      legend.title = element_text(
+        size = base_size * 0.8,
+        face = "bold",
+        hjust = 0,
+        color = "black"
+      ),
+      legend.position = "right",
+      legend.text.align = NULL,
+      legend.title.align = NULL,
+      legend.direction = "vertical",
+      legend.box = NULL,
+      # Specify panel options
+      panel.background = element_rect(fill = NA, 
+                                      color  =  NA),
+      panel.border = element_rect(fill = NA, 
+                                  color = "black",
+                                  size = 1.0),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.spacing = unit(0.5, "lines"),
+      # Specify facetting options
+      strip.background = element_rect(fill = "transparent", 
+                                      color = "grey10",
+                                      size = 1),
+      strip.text.x = element_text(size = base_size * 0.8, 
+                                  color = "black"),
+      strip.text.y = element_text(
+        size = base_size * 0.8,
+        color = "black",
+        angle = -90
+      ),
+      # Specify plot options
+      plot.background = element_rect(color = "black", 
+                                     fill = "white"),
+      plot.title = element_text(size = base_size * 1.2, 
+                                color = "black"),
+      plot.margin = unit(rep(1, 4), "lines")
+      
+    )
+  
+}
+
+
---
+++ spp_trends_descriptions.md
@@ -0,0 +1,24 @@
+_B. affinis_
+- Probability of occurrence increases over time
+- Proportion agriculture better as time goes on
+- Especially with increasing crop diversity
+
+_B. pensylvanicus_
+- Increasing sensitivity to increasing agriculture over time.
+  - More ag is good in late 19th and early 20th century
+- Generally higher occurrence in areas with fewer crops in those time periods
+
+_B. terricola_
+- Strong negative response to increasing proportion cropland, but less so recently (may be artifact of BBW)
+- Recent trends largely driven by an increase in collection effort.
+
+_B. bimaculatus_
+- Lessening sensitivity to ag over time.
+- Increasing crop diversity good late century and beyond
+
+_B. griseocollis_
+- No general trend with increasing proportion ag, execpt during recent time periods
+- Better in high ag, low richness areas now
+
+_B. impatiens_
+- Formerly high occurrence in areas with large ag extent and low crop diversity (similar to pensyl.). Now, only high occurrence in high ag provided there is higher diversity of crops.
